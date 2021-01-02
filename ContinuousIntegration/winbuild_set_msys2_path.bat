8cde41297c817f27af629589aaaa103d5d1f5664
Peter Helcmanovsky (Ped) <ped@7gods.org>
2021-01-02 12:35:17 +0100

    Cirrus CI: debugging win builds on new win images

--- ContinuousIntegration/winbuild_set_msys2_path.bat
+++ ContinuousIntegration/winbuild_set_msys2_path.bat
@@ -1,8 +1,12 @@
 rem Borrow bash and other tools from MSYS2 (and put them ahead of other MS crap)
-set PATH=C:\tools\msys64\usr\bin\;%PATH%;c:\tools\sjasmplus
+rem set PATH=C:\tools\msys64\usr\bin\;%PATH%;c:\tools\sjasmplus
+
+rem add sjasmplus to path, test availability and version of all required tooling
+set PATH=%PATH%;c:\tools\sjasmplus
 bash --version
 diff --version
 cmp --version
+where find
 find --version
 gcc --version
 mingw32-make --version
